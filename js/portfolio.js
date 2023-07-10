a96292fe7e21d42b47941e8694ba2ea5b1b1f480
leekiwon31221213 <129711547+leekiwon31221213@users.noreply.github.com>
2023-07-10 10:58:28 +0900

    [update] 일요일 작업
    일요일 작업

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,19 @@
-let skill_graph = document.querySelector(".skill_inner");
+let skill_graph = document.querySelector('.skill_inner');
+let profile_img = document.querySelector('.profile-img');
 
-window.addEventListener("scroll", function () {
-  let sec1_Height = document.querySelector(".section1-title").clientHeight;
-  let one_depth_Height = document.querySelector(".one-depth").clientHeight;
-  let two_depth_Height = document.querySelector(".two-depth").clientHeight;
+window.addEventListener('scroll', function () {
+    let sec1_Height = document.querySelector('.section1-title').clientHeight;
+    let one_depth_Height = document.querySelector('.one-depth').clientHeight;
 
-  if (window.scrollY >= sec1_Height + one_depth_Height / 2) {
-    skill_graph.style.display = "block";
-    skill_graph.style.opacity = "1";
-  } else {
-    skill_graph.style.display = "none";
-    skill_graph.style.opacity = "0";
-  }
-  console.log(sec1_Height);
+    if (window.scrollY >= sec1_Height + one_depth_Height / 2) {
+        skill_graph.style.display = 'block';
+        skill_graph.classList.add('active');
+        profile_img.style.display = 'block';
+        profile_img.classList.add('active');
+    } else {
+        skill_graph.style.display = 'none';
+        skill_graph.classList.remove('active');
+        profile_img.style.display = 'none';
+        profile_img.classList.remove('active');
+    }
 });
